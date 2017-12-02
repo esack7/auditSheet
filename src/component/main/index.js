7d832da057e92704c1730bbd41a3c521c42699b9
esack7 <isaacheist@gmail.com>
2017-12-02 15:14:29 -0800

    working on wireframe of app

---
+++ src/component/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.scss';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>This is the Main</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
